780718fb88410e18e3ab779e6daa46e42a29e695
Darragh Hanley <darragh.hanley@gmail.com>
2020-12-12 13:01:16 +0000

    Transformer 2

--- scripts/xfrmr/xfrmr_02.py
+++ scripts/xfrmr/xfrmr_02.py
@@ -260,7 +260,7 @@ DECAY = 0.0
 # Should we be stepping; all 0's first, then all 1's, then all 2,s 
 trndataset = self = SAKTDataset(train, MODCOLS, PADVALS, EXTRACOLS)
 valdataset = SAKTDataset(valid, MODCOLS, PADVALS, EXTRACOLS)
-loaderargs = {'num_workers' : 16, 'batch_size' : 256*16}
+loaderargs = {'num_workers' : 16, 'batch_size' : 256}
 trnloader = DataLoader(trndataset, shuffle=True, **loaderargs)
 valloader = DataLoader(valdataset, shuffle=False, **loaderargs)
 # x, y = next(iter(trnloader))
