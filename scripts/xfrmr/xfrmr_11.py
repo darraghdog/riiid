f74558d947f4f08b25df3955387b5b4167dc41b7
Darragh Hanley <darragh.hanley@gmail.com>
2020-12-21 14:46:06 +0000

    Correct version on lectures

--- scripts/xfrmr/xfrmr_11.py
+++ scripts/xfrmr/xfrmr_11.py
@@ -166,7 +166,7 @@ logger.info(args)
 device = 'cpu' if platform.system() == 'Darwin' else 'cuda'
 CUT=0
 DIR=args.dir#'val'
-VERSION='V10'#args.version
+VERSION='V11'#args.version
 debug = False
 validaten_flg = False
 
