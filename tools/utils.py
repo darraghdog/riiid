e66ea7ff4fffe9d999b6766671aa3264de61f340
Darragh Hanley <darragh.hanley@gmail.com>
2020-12-29 10:16:32 +0000

    Jit training

--- tools/utils.py
+++ tools/utils.py
@@ -35,6 +35,63 @@ from torch.optim.adamw import AdamW
 from torch.optim.lr_scheduler import MultiStepLR, CyclicLR
 from torch.optim.lr_scheduler import MultiStepLR, CyclicLR, StepLR
 
+import torch
+import torch.nn as nn
+from torch.nn import Parameter
+import torch.jit as jit
+import warnings
+from collections import namedtuple
+from typing import List, Tuple
+from torch import Tensor
+import numbers
+
+from torch.optim.lr_scheduler import _LRScheduler
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+# https://github.com/pytorch/pytorch/blob/master/benchmarks/fastrnns/custom_lstms.py
+class LSTMCell(jit.ScriptModule):
+    def __init__(self, input_size, hidden_size):
+        super(LSTMCell, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.weight_ih = Parameter(torch.randn(4 * hidden_size, input_size))
+        self.weight_hh = Parameter(torch.randn(4 * hidden_size, hidden_size))
+        self.bias_ih = Parameter(torch.randn(4 * hidden_size))
+        self.bias_hh = Parameter(torch.randn(4 * hidden_size))
+
+    @jit.script_method
+    def forward(self, input, state):
+        # type: (Tensor, Tuple[Tensor, Tensor]) -> Tuple[Tensor, Tuple[Tensor, Tensor]]
+        hx, cx = state
+        gates = (torch.mm(input, self.weight_ih.t()) + self.bias_ih +
+                 torch.mm(hx, self.weight_hh.t()) + self.bias_hh)
+        ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
+
+        ingate = torch.sigmoid(ingate)
+        forgetgate = torch.sigmoid(forgetgate)
+        cellgate = torch.tanh(cellgate)
+        outgate = torch.sigmoid(outgate)
+
+        cy = (forgetgate * cx) + (ingate * cellgate)
+        hy = outgate * torch.tanh(cy)
+
+        return hy, (hy, cy)
+
+class LSTMLayer(jit.ScriptModule):
+    def __init__(self, cell, *cell_args):
+        super(LSTMLayer, self).__init__()
+        self.cell = cell(*cell_args)
+
+    @jit.script_method
+    def forward(self, input, state):
+        # type: (Tensor, Tuple[Tensor, Tensor]) -> Tuple[Tensor, Tuple[Tensor, Tensor]]
+        inputs = input.unbind(0)
+        outputs = torch.jit.annotate(List[Tensor], [])
+        for i in range(len(inputs)):
+            out, state = self.cell(inputs[i], state)
+            outputs += [out]
+        return torch.stack(outputs), state
+
 def split_tags(x) : 
     if x == '': return [188]*6
     return list(map(int, x.split(' ')))+[188]*(6-len(x.split(' '))) 
