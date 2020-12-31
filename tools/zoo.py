b7baf35825643822f7a304fbf40ca17128b1a876
Darragh Hanley <darragh.hanley@gmail.com>
2020-12-31 12:52:13 +0000

    Add spatial dropout

--- tools/zoo.py
+++ tools/zoo.py
@@ -15,8 +15,15 @@ import torch.nn.functional as F
 from torch import nn
 #from pytorch_transformers.modeling_bert import BertConfig, BertEncoder
 from transformers import XLMModel, XLMConfig
-from tools.utils import get_logger, SpatialDropout, split_tags
 
+class SpatialDropout(nn.Dropout2d):
+    def forward(self, x):
+        x = x.unsqueeze(2)    # (N, T, 1, K)
+        x = x.permute(0, 3, 2, 1)  # (N, K, 1, T)
+        x = super(SpatialDropout, self).forward(x)  # (N, K, 1, T), some features are masked
+        x = x.permute(0, 3, 2, 1)  # (N, T, 1, K)
+        x = x.squeeze(2)  # (N, T, K)
+        return x
 
 # inputs, lengths = hiddenq, m.sum(1)
 class Attention21(nn.Module):
