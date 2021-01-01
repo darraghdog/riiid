31e1e18163a577942b0e1e6b870ad498efc1a56f
Darragh Hanley <darragh.hanley@gmail.com>
2021-01-01 12:19:40 +0000

    Stack with best public models

--- scripts/xfrmr/stack_01.py
+++ scripts/xfrmr/stack_01.py
@@ -561,7 +561,7 @@ if args.infer:
                f'data/{DIR}/{VERSION}/basemodels/lstm_V20_hidden512_ep4.bin', 
                f'data/{DIR}/{VERSION}/basemodels/lstm_V20_hidden512_ep5.bin', 
                f'data/{DIR}/{VERSION}/basemodels/lstm_V20_hidden512_ep6.bin', 
-               f'data/{DIR}/{VERSION}/basemodels/lstm_V20_hidden512_ep7.bin' 
+               f'data/{DIR}/{VERSION}/basemodels/lstm_V20_hidden512_ep7.bin',
                f'data/{DIR}/{VERSION}/basemodels/lstm_V21_hidden512_ep3.bin', 
                f'data/{DIR}/{VERSION}/basemodels/lstm_V24_hidden512_ep3.bin']
     mkeys = ['V12_6', 'V12_7', 'V20_3', 'V20_4', 'V20_5', 'V20_6', 'V20_7', 'V21_3', 'V24_3']
