6352a13eae3180fe0236a07ce4cb45f83a0c1b05
Darragh Hanley <darragh.hanley@gmail.com>
2021-01-01 18:10:32 +0000

    New models in stack

--- scripts/xfrmr/stack_01.py
+++ scripts/xfrmr/stack_01.py
@@ -563,8 +563,11 @@ if args.infer:
                f'data/{DIR}/{VERSION}/basemodels/lstm_V20_hidden512_ep6.bin', 
                f'data/{DIR}/{VERSION}/basemodels/lstm_V20_hidden512_ep7.bin',
                f'data/{DIR}/{VERSION}/basemodels/lstm_V21_hidden512_ep3.bin', 
-               f'data/{DIR}/{VERSION}/basemodels/lstm_V24_hidden512_ep3.bin']
-    mkeys = ['V12_6', 'V12_7', 'V20_3', 'V20_4', 'V20_5', 'V20_6', 'V20_7', 'V21_3', 'V24_3']
+               f'data/{DIR}/{VERSION}/basemodels/lstm_V24_hidden512_ep3.bin',
+               f'data/{DIR}/{VERSION}/basemodels/lstm_V27_hidden512_ep3.bin', 
+               f'data/{DIR}/{VERSION}/basemodels/lstm_V28_hidden512_ep3.bin', 
+               ]
+    mkeys = ['V12_6', 'V12_7', 'V20_3', 'V20_4', 'V20_5', 'V20_6', 'V20_7', 'V21_3', 'V24_3', 'V27_3', 'V28_3']
     modeldict = dict((k,load_model_weights(modfn, wtname, laargs)) \
                      for (k,modfn, wtname) in zip(mkeys,modfns, wtnames))
     '''
--- tools/zoo.py
+++ tools/zoo.py
@@ -653,3 +653,235 @@ class LearnNet14(nn.Module):
         out = self.linear_out(hidden[:,-1,:]).flatten()
         
         return out
+
+class LearnNet27(nn.Module):
+    def __init__(self, modcols, contcols, padvals, extracols, 
+                 device = device, dropout = 0.2, model_type = args.model, 
+                 hidden = args.hidden):
+        super(LearnNet27, self).__init__()
+        
+        self.dropout = nn.Dropout(dropout)
+        
+        self.padvals = padvals
+        self.extracols = extracols
+        self.modcols = modcols + extracols
+        self.contcols = contcols
+        self.embcols = ['content_id', 'part']
+        self.model_type = model_type
+        
+        self.emb_content_id = nn.Embedding(13526, 32)
+        self.emb_content_id_prior = nn.Embedding(13526*3, 32)
+        self.emb_bundle_id = nn.Embedding(13526, 32)
+        self.emb_part = nn.Embedding(9, 4)
+        self.emb_tag= nn.Embedding(190, 16)
+        self.emb_lpart = nn.Embedding(9, 4)
+        self.emb_prior = nn.Embedding(3, 2)
+        self.emb_ltag= nn.Embedding(190, 16)
+        self.emb_lag_time = nn.Embedding(301, 16)
+        self.emb_elapsed_time = nn.Embedding(301, 16)
+        self.emb_cont_user_answer = nn.Embedding(13526 * 4, 5)
+            
+        self.tag_idx = torch.tensor(['tag' in i for i in self.modcols])
+        self.tag_wts = torch.ones((sum(self.tag_idx), 16))  / sum(self.tag_idx)
+        self.tag_wts = nn.Parameter(self.tag_wts)
+        self.tag_wts.requires_grad = True
+        self.cont_wts = nn.Parameter( torch.ones(len(self.contcols)) )
+        self.cont_wts.requires_grad = True
+        
+        self.cont_idx = [self.modcols.index(c) for c in self.contcols]
+        self.cont_idxcts = [t for t,c in enumerate(self.contcols) if 'counts' in c]
+        
+        self.embedding_dropout = SpatialDropout(dropout)
+        
+        self.diffsize = self.emb_content_id.embedding_dim + self.emb_part.embedding_dim + \
+                        self.emb_bundle_id.embedding_dim + self.emb_tag.embedding_dim * 7 
+        IN_UNITSQ = self.diffsize * 2 + \
+                    self.emb_lpart.embedding_dim + self.emb_ltag.embedding_dim + \
+                        self.emb_prior.embedding_dim + self.emb_content_id_prior.embedding_dim + \
+                        len(self.cont_idxcts)
+        IN_UNITSQA = 2 * ( self.emb_lag_time.embedding_dim + self.emb_elapsed_time.embedding_dim + \
+                self.emb_cont_user_answer.embedding_dim) + len(self.contcols)
+        LSTM_UNITS = hidden 
+        self.diffsize = self.emb_content_id.embedding_dim + self.emb_part.embedding_dim + \
+                        self.emb_bundle_id.embedding_dim + self.emb_tag.embedding_dim * 7 
+        
+        self.seqnet1 = nn.LSTM(IN_UNITSQ, LSTM_UNITS, bidirectional=False, batch_first=True)
+        self.seqnet2 = nn.LSTM(IN_UNITSQA + LSTM_UNITS, LSTM_UNITS, bidirectional=False, batch_first=True)
+            
+        self.linear1 = nn.Linear(LSTM_UNITS, LSTM_UNITS//2)
+        self.bn0 = nn.BatchNorm1d(num_features=len(self.contcols))
+        self.bn1 = nn.BatchNorm1d(num_features=LSTM_UNITS)
+        self.bn2 = nn.BatchNorm1d(num_features=LSTM_UNITS//2)
+        
+        self.linear_out = nn.Linear(LSTM_UNITS//2, 1)
+        
+    def forward(self, x, m = None):
+        
+        ## Continuous
+        contmat  = x[:,:, self.cont_idx]
+        contmat = self.bn0(contmat.permute(0,2,1)) .permute(0,2,1)
+        contmat = contmat * self.cont_wts
+        
+        content_id_prior = x[:,:,self.modcols.index('content_id')] * 3 + \
+                            x[:,:, self.modcols.index('prior_question_had_explanation')]
+        embcatq = torch.cat([
+            self.emb_content_id(  x[:,:, self.modcols.index('content_id')].long()  ),
+            self.emb_part(  x[:,:, self.modcols.index('part')].long()  ), 
+            self.emb_bundle_id(  x[:,:, self.modcols.index('bundle_id')].long()  ),
+            self.emb_tag(x[:,:, self.tag_idx].long()).view(x.shape[0], x.shape[1], -1),
+            self.emb_prior( x[:,:, self.modcols.index('prior_question_had_explanation')].long() ),
+            #self.emb_cont_user_answer(  x[:,:, self.modcols.index('content_user_answer')].long()  ),
+            self.emb_lpart(  x[:,:, self.modcols.index('lecture_part')].long()  ), 
+            self.emb_ltag(  x[:,:, self.modcols.index('lecture_tag')].long()  ) , 
+            self.emb_content_id_prior(  content_id_prior.long()  ),
+            #self.emb_lag_time(   x[:,:, self.modcols.index('lag_time_cat')].long()   ), 
+            #self.emb_elapsed_time(  x[:,:, self.modcols.index('elapsed_time_cat')].long()  )
+            ] #+ [self.emb_tag(x[:,:, ii.item()].long()) for ii in torch.where(self.tag_idx)[0]]
+            , 2)
+        embcatqdiff = embcatq[:,:,:self.diffsize] - embcatq[:,-1,:self.diffsize].unsqueeze(1)
+            
+        # Categroical embeddings
+        embcatqa = torch.cat([
+            #self.emb_content_id(  x[:,:, self.modcols.index('content_id')].long()  ),
+            #self.emb_bundle_id(  x[:,:, self.modcols.index('bundle_id')].long()  ),
+            self.emb_cont_user_answer(  x[:,:, self.modcols.index('content_user_answer')].long()  ),
+            self.emb_lag_time(   x[:,:, self.modcols.index('lag_time_cat')].long()   ), 
+            self.emb_elapsed_time(  x[:,:, self.modcols.index('elapsed_time_cat')].long()  )
+            ] #+ [self.emb_tag(x[:,:, ii.item()].long()) for ii in torch.where(self.tag_idx)[0]]
+            , 2)
+        embcatqadiff = embcatqa - embcatqa[:,-1].unsqueeze(1)
+        embcatq = self.embedding_dropout(embcatq)
+        embcatqa = self.embedding_dropout(embcatqa)
+        embcatqdiff = self.embedding_dropout(embcatqdiff)
+        embcatqadiff = self.embedding_dropout(embcatqadiff)
+        
+        # Weighted sum of tags - hopefully good weights are learnt
+        xinpq = torch.cat([embcatq, embcatqdiff, contmat[:,:,self.cont_idxcts]], 2)
+        hiddenq, _ = self.seqnet1(xinpq)
+        xinpqa = torch.cat([embcatqa, embcatqadiff, contmat, hiddenq], 2)
+        hiddenqa, _ = self.seqnet2(xinpqa)
+        
+        # Take last hidden unit
+        hidden = hiddenqa[:,-1,:]
+        hidden = self.dropout( self.bn1( hidden) )
+        hidden  = F.relu(self.linear1(hidden))
+        hidden = self.dropout(self.bn2(hidden))
+        out = self.linear_out(hidden).flatten()
+        
+        return out
+
+class LearnNet28(nn.Module):
+    def __init__(self, modcols, contcols, padvals, extracols, 
+                 device = device, dropout = 0.2, model_type = args.model, 
+                 hidden = args.hidden):
+        super(LearnNet28, self).__init__()
+        
+        self.dropout = nn.Dropout(dropout)
+        
+        self.padvals = padvals
+        self.extracols = extracols
+        self.modcols = modcols + extracols
+        self.contcols = contcols
+        self.embcols = ['content_id', 'part']
+        self.model_type = model_type
+        
+        self.emb_content_id = nn.Embedding(13526, 32)
+        self.emb_content_id_prior = nn.Embedding(13526*3, 32)
+        self.emb_bundle_id = nn.Embedding(13526, 32)
+        self.emb_part = nn.Embedding(9, 4)
+        self.emb_tag= nn.Embedding(190, 8)
+        self.emb_lpart = nn.Embedding(9, 4)
+        self.emb_prior = nn.Embedding(3, 2)
+        self.emb_ltag= nn.Embedding(190, 16)
+        self.emb_lag_time = nn.Embedding(301, 16)
+        self.emb_elapsed_time = nn.Embedding(301, 16)
+        self.emb_cont_user_answer = nn.Embedding(13526 * 4, 5)
+            
+        self.tag_idx = torch.tensor(['tag' in i for i in self.modcols])
+        self.tag_wts = torch.ones((sum(self.tag_idx), 16))  / sum(self.tag_idx)
+        self.tag_wts = nn.Parameter(self.tag_wts)
+        self.tag_wts.requires_grad = True
+        self.cont_wts = nn.Parameter( torch.ones(len(self.contcols)) )
+        self.cont_wts.requires_grad = True
+        
+        self.cont_idx = [self.modcols.index(c) for c in self.contcols]
+        self.cont_idxcts = [t for t,c in enumerate(self.contcols) if 'counts' in c]
+        
+        self.embedding_dropout = SpatialDropout(dropout)
+        
+        self.diffsize = self.emb_content_id.embedding_dim + self.emb_part.embedding_dim + \
+                        self.emb_bundle_id.embedding_dim + self.emb_tag.embedding_dim * 7 
+        IN_UNITSQ = self.diffsize * 2 + \
+                    self.emb_lpart.embedding_dim + self.emb_ltag.embedding_dim + \
+                        self.emb_prior.embedding_dim + self.emb_content_id_prior.embedding_dim + \
+                        len(self.cont_idxcts)
+        IN_UNITSQA = ( self.emb_lag_time.embedding_dim + self.emb_elapsed_time.embedding_dim + \
+                self.emb_cont_user_answer.embedding_dim) + len(self.contcols)
+        LSTM_UNITS = hidden 
+        self.diffsize = self.emb_content_id.embedding_dim + self.emb_part.embedding_dim + \
+                        self.emb_bundle_id.embedding_dim + self.emb_tag.embedding_dim * 7 
+        
+        self.seqnet1 = nn.LSTM(IN_UNITSQ, LSTM_UNITS, bidirectional=False, batch_first=True)
+        self.seqnet2 = nn.LSTM(IN_UNITSQA + LSTM_UNITS, LSTM_UNITS, bidirectional=False, batch_first=True)
+            
+        self.linear1 = nn.Linear(LSTM_UNITS * 2 + len(self.contcols), LSTM_UNITS//2)
+        self.bn0 = nn.BatchNorm1d(num_features=len(self.contcols))
+        self.bn1 = nn.BatchNorm1d(num_features=LSTM_UNITS * 2 + len(self.contcols))
+        self.bn2 = nn.BatchNorm1d(num_features=LSTM_UNITS//2)
+        
+        self.linear_out = nn.Linear(LSTM_UNITS//2, 1)
+        
+    def forward(self, x, m = None):
+        
+        ## Continuous
+        contmat  = x[:,:, self.cont_idx]
+        contmat = self.bn0(contmat.permute(0,2,1)) .permute(0,2,1)
+        contmat = contmat * self.cont_wts
+        
+        content_id_prior = x[:,:,self.modcols.index('content_id')] * 3 + \
+                            x[:,:, self.modcols.index('prior_question_had_explanation')]
+        embcatq = torch.cat([
+            self.emb_content_id(  x[:,:, self.modcols.index('content_id')].long()  ),
+            self.emb_part(  x[:,:, self.modcols.index('part')].long()  ), 
+            self.emb_bundle_id(  x[:,:, self.modcols.index('bundle_id')].long()  ),
+            self.emb_tag(x[:,:, self.tag_idx].long()).view(x.shape[0], x.shape[1], -1),
+            self.emb_prior( x[:,:, self.modcols.index('prior_question_had_explanation')].long() ),
+            #self.emb_cont_user_answer(  x[:,:, self.modcols.index('content_user_answer')].long()  ),
+            self.emb_lpart(  x[:,:, self.modcols.index('lecture_part')].long()  ), 
+            self.emb_ltag(  x[:,:, self.modcols.index('lecture_tag')].long()  ) , 
+            self.emb_content_id_prior(  content_id_prior.long()  ),
+            #self.emb_lag_time(   x[:,:, self.modcols.index('lag_time_cat')].long()   ), 
+            #self.emb_elapsed_time(  x[:,:, self.modcols.index('elapsed_time_cat')].long()  )
+            ] #+ [self.emb_tag(x[:,:, ii.item()].long()) for ii in torch.where(self.tag_idx)[0]]
+            , 2)
+        embcatqdiff = embcatq[:,:,:self.diffsize] - embcatq[:,-1,:self.diffsize].unsqueeze(1)
+            
+        # Categroical embeddings
+        embcatqa = torch.cat([
+            #self.emb_content_id(  x[:,:, self.modcols.index('content_id')].long()  ),
+            #self.emb_bundle_id(  x[:,:, self.modcols.index('bundle_id')].long()  ),
+            self.emb_cont_user_answer(  x[:,:, self.modcols.index('content_user_answer')].long()  ),
+            self.emb_lag_time(   x[:,:, self.modcols.index('lag_time_cat')].long()   ), 
+            self.emb_elapsed_time(  x[:,:, self.modcols.index('elapsed_time_cat')].long()  )
+            ] #+ [self.emb_tag(x[:,:, ii.item()].long()) for ii in torch.where(self.tag_idx)[0]]
+            , 2)
+        #embcatqadiff = embcatqa - embcatqa[:,-1].unsqueeze(1)
+        embcatq = self.embedding_dropout(embcatq)
+        embcatqa = self.embedding_dropout(embcatqa)
+        embcatqdiff = self.embedding_dropout(embcatqdiff)
+        #embcatqadiff = self.embedding_dropout(embcatqadiff)
+        
+        # Weighted sum of tags - hopefully good weights are learnt
+        xinpq = torch.cat([embcatq, embcatqdiff, contmat[:,:,self.cont_idxcts]], 2)
+        hiddenq, _ = self.seqnet1(xinpq)
+        xinpqa = torch.cat([embcatqa, contmat, hiddenq], 2)
+        hiddenqa, _ = self.seqnet2(xinpqa)
+        
+        # Take last hidden unit
+        hidden = torch.cat([hiddenqa[:,-1,:], hiddenq[:,-1,:], contmat[:, -1]], 1)
+        hidden = self.dropout( self.bn1( hidden) )
+        hidden  = F.relu(self.linear1(hidden))
+        hidden = self.dropout(self.bn2(hidden))
+        out = self.linear_out(hidden).flatten()
+        
+        return out
